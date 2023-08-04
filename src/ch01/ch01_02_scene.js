0b57b766c27781ca3185904cdd1268afdb231f1f
Mr.Moon <MrMoonKr@gmail.com>
2023-08-04 18:13:45 +0900

    전체 애플리케이션 구조 잡음...

--- src/ch01/ch01_02_scene.js
+++ src/ch01/ch01_02_scene.js
@@ -22,19 +22,47 @@ class App
          * @type {THREE.WebGLRenderer}
          */
         this.renderer   = undefined ;
+        /**
+         * @type {THREE.Scene}
+         */
+        this.scene      = undefined ;
+        /**
+         * @type {THREE.PerspectiveCamera}
+         */
+        this.camera     = undefined ;
+
+
 
         this._setupRenderer() ;
+        this._setupScene() ;
+        this._setupCamera() ;
 
+    }
+
+    onUpdate( deltaTime )
+    {
+        //console.log( `[App] onUpdate() 호출 : ${deltaTime}` ) ;
+        const cube = this.scene.getObjectByName( 'cube' );
+        if ( cube )
+        {
+            cube.rotateY( 3 * Math.PI / 180 ) ;
+        }
     }
 
     onRender()
     {
-        console.log( `[App] render() called : 캔버스 ${this.canvas.width}x${this.canvas.height}` );
+        //console.log( `[App] render() called : 캔버스 ${this.canvas.width}x${this.canvas.height}` );
+
+        this.renderer.render( this.scene, this.camera ) ;
     }
 
     onWindowResize()
     {
-        this.renderer.setSize( window.innerWidth, window.innerHeight * 0.9 );
+        this.renderer.setSize( window.innerWidth, window.innerHeight * 0.9 ) ;
+        this.renderer.setPixelRatio( window.devicePixelRatio ) ;
+
+        this.camera.aspect = this.canvas.width / this.canvas.height ;
+        this.camera.updateProjectionMatrix() ;
     }
 
     _setupRenderer()
@@ -42,12 +70,49 @@ class App
         this.renderer = new THREE.WebGLRenderer( { canvas: this.canvas } );
         this.renderer.setPixelRatio( window.devicePixelRatio );
         //this.renderer.setClearColor( 0xFF00FF );
-        this.renderer.setClearColor( new THREE.Color( 0.2, 0.2, 0.2 ) );
+        this.renderer.setClearColor( new THREE.Color( 0.1, 0.1, 0.1 ) );
         this.renderer.clearColor();
+    }
+
+    _setupScene()
+    {
+        this.scene = new THREE.Scene() ;
+
+        const axis = new THREE.AxesHelper( 1 ) ;
+        this.scene.add( axis ) ;
+
+        const cubeGeo = new THREE.BoxGeometry( 1, 1, 1 ) ;
+        const cubeMtl = new THREE.MeshBasicMaterial( {
+            color: 0xFF0000,
+            wireframe: true
+        } ) ;
+
+        const cube = new THREE.Mesh( cubeGeo, cubeMtl ) ;
+        cube.name = 'cube' ;
+        //cube.position.set( 0, 0, 1 ) ;
+        this.scene.add( cube ) ;
+    }
+
+    _setupCamera()
+    {
+        this.camera = new THREE.PerspectiveCamera(
+            50 ,
+            this.canvas.width / this.canvas.height ,
+            0.1 ,
+            3000
+        ) ;
+
+        this.camera.position.set( 5, 10, 10 ) ;
+        this.camera.lookAt( this.scene.position ) ;
     }
 }
 
 // 웹 시작 및 웹 메인 루프
+
+let _start     = performance.now() ;
+let _previous  = _start ;
+let _elapsed   = 0 ;
+let _delta     = 0 ;
 
 /**
  * 하트비트 메인 시뮬레이션 루프 함수
@@ -55,6 +120,12 @@ class App
  */
 function mainLoop( timeStamp )
 {
+    const currentTick = performance.now() ;
+    _delta      = ( currentTick - _previous ) / 1000 ;
+    _previous   = currentTick ;
+    _elapsed    = currentTick - _start ;
+
+    theApp.onUpdate( _delta ) ;
     theApp.onRender();
     
     //console.log( '[정보] timeStamp : ' + parseInt( timeStamp / 1000 ) );
